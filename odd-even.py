129e65828c87229c86898826e8f0589c7ff75c5a
Jaya Keerthy <jaikeerthi@gmail.com>
2018-07-17 17:07:58 +0530

    odd/even version 2

--- odd-even.py
+++ odd-even.py
@@ -1,8 +1,16 @@
 #to check the entered number is a ODD number or even number
 
-num = int(input("Enter a number: "))
-mod = num % 2
-if mod > 0:
-    print("You picked an odd number.")
+num = int(input("give me a number to check: "))
+check = int(input("give me a number to divide by: "))
+
+if num % 4 == 0:
+    print(num, "is a multiple of 4")
+elif num % 2 == 0:
+    print(num, "is an even number")
 else:
-    print("You picked an even number.")
+    print(num, "is an odd number")
+
+if num % check == 0:
+    print(num, "divides evenly by", check)
+else:
+    print(num, "does not divide evenly by", check)
